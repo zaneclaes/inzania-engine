e6fc7e4c048a83f7f820922696dc118e9c99c4b6
Zane Claes <zane@claes.biz>
2025-07-09 18:03:10 -0600

    Controller response model

---
+++ ZCore/Observability/ZApiControllerResponse.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using IZ.Core.Data;
+
+namespace IZ.Core.Observability;
+
+public class ZApiControllerError : TransientObject {
+  public string Message { get; set; } = null!;
+
+  public string Type { get; set; } = null!;
+
+  public string Code { get; set; } = null!;
+
+  // Data & Stack also exist...
+}
+
+
+public class ZApiControllerMeta : TransientObject {
+  public string TraceId { get; set; } = null!;
+}
+
+public class ZApiControllerResponse : TransientObject {
+  public List<ZApiControllerError> Errors { get; set; } = new List<ZApiControllerError>();
+
+  public ZApiControllerMeta? Meta { get; set; }
+}
