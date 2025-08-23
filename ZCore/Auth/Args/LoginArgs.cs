bf96b1840caeb8816dc2f5a02fd78bcc1b310b4d
Zane Claes <zane@claes.biz>
2025-08-22 19:25:01 -0600

    Signup annotations

--- ZCore/Auth/Args/LoginArgs.cs
+++ ZCore/Auth/Args/LoginArgs.cs
@@ -1,5 +1,6 @@
 #region
 
+using System.ComponentModel.DataAnnotations;
 using IZ.Core.Data;
 
 #endregion
@@ -9,5 +10,14 @@ namespace IZ.Core.Auth.Args;
 public class LoginArgs : TransientObject {
   public string Username { get; set; } = null!;
 
+  public string Password { get; set; } = null!;
+}
+
+public class AuthLoginArgs {
+  [Required]
+  [StringLength(32, ErrorMessage = "Username must be 4-32 characters long.", MinimumLength = 4)]
+  public string Username { get; set; } = null!;
+
+  [Required]
   public string Password { get; set; } = null!;
 }
--- ZCore/Auth/Args/SignUpArgs.cs
+++ ZCore/Auth/Args/SignUpArgs.cs
@@ -1,5 +1,7 @@
 #region
 
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using IZ.Core.Data;
 
 #endregion
@@ -15,9 +17,19 @@ public class SignUpArgs : TransientObject {
 }
 
 public class AuthSignUpArgs {
+  [Required]
+  [EmailAddress]
   public string Email { get; set; } = null!;
 
+  [Required]
+  [StringLength(32, ErrorMessage = "Username must be 4-32 characters long.", MinimumLength = 4)]
   public string Username { get; set; } = null!;
 
+  [Required]
+  [StringLength(32, ErrorMessage = "Password must be 6-32 characters long.", MinimumLength = 6)]
   public string Password { get; set; } = null!;
+
+  [Required]
+  [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+  public string PasswordAgain { get; set; } = null!;
 }
