6947900f36598e6d41751654bd2afe8e05fa449d
Zane Claes <zane@claes.biz>
2025-10-02 21:33:59 -0600

    Packet API

--- ZP2P/Packets/MessagePacketSerializer.cs
+++ ZP2P/Packets/MessagePacketSerializer.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using IZ.Core.Contexts;
+using IZ.Core.Data;
 using MessagePack;
 
 namespace IZ.P2P.Packets;
@@ -19,6 +20,9 @@ public class MessagePacketSerializer : LogicBase, IZPacketSerializer {
   public byte[] SerializePacketData<TPacket>(TPacket packet) where TPacket : ZPacket =>
     MessagePackSerializer.Serialize<ZPacket>(packet, _options);
 
+  public byte[] SerializeApiData<TPacket>(TPacket packet) where TPacket : ApiObject =>
+    MessagePackSerializer.Serialize<ApiObject>(packet, _options);
+
   public async Task<TPacket> DeserializePacketStream<TPacket>(IZContext context, Stream stream) where TPacket : ZPacket {
     var ret = await MessagePackSerializer.DeserializeAsync<ZPacket>(stream, _options);
     ret.Context = context;
@@ -26,6 +30,12 @@ public class MessagePacketSerializer : LogicBase, IZPacketSerializer {
   }
 
   public TPacket DeserializePacketData<TPacket>(IZContext context, byte[] data) where TPacket : ZPacket {
+    var ret = MessagePackSerializer.Deserialize<ZPacket>(data, _options);
+    ret.Context = context;
+    return ret as TPacket ?? throw new SystemException($"Could not convert packet {ret.GetType()} to {typeof(TPacket)}");
+  }
+
+  public TPacket DeserializeApiData<TPacket>(IZContext context, byte[] data) where TPacket : ApiObject {
     var ret = MessagePackSerializer.Deserialize<ZPacket>(data, _options);
     ret.Context = context;
     return ret as TPacket ?? throw new SystemException($"Could not convert packet {ret.GetType()} to {typeof(TPacket)}");
