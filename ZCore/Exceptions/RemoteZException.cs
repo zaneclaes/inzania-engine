4cc57dd32b1dec63e5786c5b232052efeaec8bd2
Zane Claes <zane@claes.biz>
2025-11-07 11:18:01 -0700

    Remote exception body

--- ZCore/Exceptions/RemoteZException.cs
+++ ZCore/Exceptions/RemoteZException.cs
@@ -10,10 +10,17 @@ namespace IZ.Core.Exceptions;
 
 [ApiDocs("A remote service failed (i.e., HTTP request)")]
 public class RemoteZException : ZException {
+  public string? ResponseBody  { get; private set; }
+
   public RemoteZException(IZContext context, string message, Exception? innerException = null) :
     base(context, message, innerException) { }
 
   public RemoteZException(IZContext context, string message, string reason) : base(context, message) {
     Reason = reason;
+  }
+
+  public RemoteZException WithResponseBody(string? body) {
+    if (body != null) ResponseBody = body;
+    return this;
   }
 }
